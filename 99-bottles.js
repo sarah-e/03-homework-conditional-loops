e845f4fa6a949751b9f6a419832741ecef077721
Sarah Esteban <sarah@viator.com>
2016-08-29 17:38:57 +1000

    add 99 bottles and random address

---
+++ 99-bottles.js
@@ -0,0 +1,7 @@
+for (var count = 99; count >= 1; count--) {
+  if (count !== 1){
+    console.log(count + ' bottles of beer on the wall');
+  } else {
+    console.log(count + ' bottle of beer on the wall');
+  }
+}
---
+++ random-address.js
@@ -0,0 +1,20 @@
+// Write a script that can generate random addresses
+// As a first step, create arrays that contain dummy data for each of the following: street number, street name, city name, state name and zip code
+// Your script should randomly select one item from each of these arrays and then use them to construct a random address
+// Each time you run the script, it should print a new randomly-generated address to the terminal. For example:
+// node random-address.js
+// => 34578 Dolphin Street, Wonka NY, 44506
+
+var number = [ 22, 15, 5, 293, 648, 89, 34, 80, 3567, 655];
+var street = ['Dolphin Street', 'Station Street', 'High Street', 'Elizabeth Street',
+            'City Road', 'Anzac Parade', 'Crown Street', 'Infinate Loop', 'Ocean Drive', 'Random Ave' ];
+var city = ['Sydney', 'Melbourne', 'Hobart', 'Perth', 'Brisbane', 'Adelaide', 'Canberra', 'Darwin', 'Cairns', 'Wagga Wagga' ];
+var postcode = [3621, 7865, 2456, 7895];
+
+var random1 = Math.floor(Math.random() * 10)
+var random2 = Math.floor(Math.random() * 10)
+var random3 = Math.floor(Math.random() * 10)
+var random4 = Math.floor(Math.random() * 4)
+
+
+console.log( number[random1] + ', ' + street[random2] + ', ' + city[random3] + ', ' + postcode[random4]);
