5ff677e2bdf8cce79acee7f18af83b3909296c90
Sarah Esteban <sarah@viator.com>
2016-08-31 16:00:31 +1000

    add more lines of the song to output

--- 99-bottles.js
+++ 99-bottles.js
@@ -1,7 +1,18 @@
-for (var count = 99; count >= 1; count--) {
-  if (count !== 1){
-    console.log(count + ' bottles of beer on the wall');
-  } else {
-    console.log(count + ' bottle of beer on the wall');
+for (var count = 99; count >= 0; count--) {
+  var countDown = count - 1
+  if (count > 1){
+    console.log(count + ' bottles of beer on the wall, ' + count + ' bottles of beer');
+    if (count > 2){
+        console.log('Take one down and pass it around, ' + countDown + ' bottles of beer on the wall');
+      } else {
+        console.log('Take one down and pass it around, ' + countDown + ' bottle of beer on the wall');
+    }
+  } else if (count === 1){
+    console.log(count + ' bottle of beer on the wall, ' + count + ' bottle of beer');
+    console.log('Take one down and pass it around, ' + countDown + ' bottle of beer on the wall');
+  }
+  if (count === 0) {
+    console.log('No more bottles of beer on the wall, no more bottles of beer. ');
+    console.log('Go to the store and buy some more, 99 bottles of beer on the wall.');
   }
 }
